c9195a1d4e15e32dd4ccdb91c34e8cdd7140233e
om2137 <omraut961@gmail.com>
2022-05-28 14:35:45 +0530

    increased solidity version

--- contract/contracts/Greeter.sol
+++ contract/contracts/Greeter.sol
@@ -1,11 +1,11 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 
 contract Greeter {
     string private greeting;
-
+ 
     constructor(string memory _greeting) {
         console.log("Deploying a Greeter with greeting:", _greeting);
         greeting = _greeting;
