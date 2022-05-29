16872a6f16613fb3ba0fb18f865a39321d72333f
om2137 <omraut961@gmail.com>
2022-05-29 15:59:33 +0530

    sign up contract

--- contract/contracts/Thought.sol
+++ contract/contracts/Thought.sol
@@ -17,7 +17,8 @@ contract Thoughts{
 
     function signup(string memory _username, string memory _name,string memory _bio,string memory _avatar) public {
         require(bytes(usernames[msg.sender]).length == 0, "check");
-        
+        require(users[_username].wallet == address(0), "Username is taken, please try another one");
+
         users[_username] = User({
             wallet: msg.sender,
             name: _name,
@@ -28,5 +29,9 @@ contract Thoughts{
         usernames[msg.sender]= _username;
 
 
+    }
+
+    function getUser(address _wallet) public view returns (User memory){
+        return users[usernames[_wallet]];
     }
 }
--- contract/test/thoughts-test.js
+++ contract/test/thoughts-test.js
@@ -8,14 +8,35 @@ describe("thought", function () {
     const thoughts = await Thoughts.deploy();
     await thoughts.deployed();
 
-    await thoughts.signup("abhi", "Abhi", "Some bio", "someUrl");
-    console.log("signing up abhi...");
+    await thoughts.signup("om", "Om", "Some bio", "someUrl");
+    console.log("signing up om...");
 
-    expect(await thoughts.usernames(user1.address)).to.equal("abhi");
+    const user = await thoughts.users("om");
+    expect(user.name).to.equal("Om");
+    expect(user.bio).to.equal("Some bio");
+    expect(user.avatar).to.equal("someUrl");
+    console.log("test signup is successful");
+
+    const userFromAddress = await thoughts.getUser(user1.address);
+    expect(userFromAddress.username).to.equal("om");
+    expect(userFromAddress.name).to.equal("Om");
+    expect(userFromAddress.bio).to.equal("Some bio");
+    expect(userFromAddress.avatar).to.equal("someUrl");
+    console.log("test signup is successful");
+
+    expect(await thoughts.usernames(user1.address)).to.equal("om");
 
     await expect(thoughts.signup("","","","")).to.be.revertedWith(
       "check"
     );
+    console.log("test user already exists error");
+
+    await expect(
+      thoughts
+        .connect(user2)
+        .signup("om", "OmRaut", "some bio", "someUrlavt")
+    ).to.be.revertedWith("Username is taken, please try another one");
+    console.log("test user already exists error");
   
   });
 });
