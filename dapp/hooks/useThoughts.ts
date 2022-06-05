a89f243b2433f26e7f198e4873c4062cc99b43e6
om2137 <omraut961@gmail.com>
2022-06-05 16:18:41 +0530

    contract key comment

--- dapp/hooks/useThoughts.ts
+++ dapp/hooks/useThoughts.ts
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { create } from 'domain';
 
 const ContractAbi = Thoughts.abi;
-const ContractAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+const ContractAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';//key of deployed contract should be changed accordingly.
 const Ethereum = typeof window !== 'undefined' && (window as any).ethereum;
 
 const getThoughtsContract = () => {
