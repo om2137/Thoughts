52c71c1f6a0cc7c3bd86998831a00775cf061d1c
Om Raut <33827410+om2137@users.noreply.github.com>
2022-06-10 14:51:36 +0530

    index changes

--- dapp/pages/index.tsx
+++ dapp/pages/index.tsx
@@ -11,7 +11,7 @@ export default function Home() {
   //console.log(user);
   const [thoughtContent, setThoughtContent] = useState<string>('');
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="flex min-h-screen flex-col items-center justify-center py-2 bg-gray-900">
       <Head>
         <title>Thoughts</title>
         <link rel="icon" href="/favicon.ico" />
@@ -19,11 +19,9 @@ export default function Home() {
       
       
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 ">
-        <h1 className="mb-8 text-6xl font-bold text-center">
+        <h1 className="mb-8 text-6xl font-bold text-center text-white">
           Welcome to <span className="text-red-400">Thoughts</span>
         </h1>
-        {/* <img src="https://i.pinimg.com/originals/a3/5f/1f/a35f1ff1fdd3a241a838ce3bdfc2f1d2.jpg" className="rounded-full h-16 w-16" alt='error' /> */}
-
         {!account ? (
         <Button label = "Connect to Etherum" onClick={connect} />
         ) : 
@@ -31,15 +29,15 @@ export default function Home() {
             <SignUpForm />
           ): (
             <>
-            <div className='flex items-center justify-center w-100 pb-4'>
-              <img src={user?.avatar}  className="rounded-full h-16 w-16 mr-2" />
+            <div className='flex items-center justify-center w-100 pb-8'>
+              <img src={user?.avatar}  className="rounded-full mr-2" width="80"/>
               <div className='flex flex-col justify-start'>
-                <span className='flex flex-col text-left'>{user?.name}</span>
+                <h1 className='flex flex-col text-left text-2xl font-bold text-white'>{user?.name}</h1>
                 <span className='text-sm text-gray-400 text-left'>@{user?.username}</span>
               </div>
             </div>
             <div className='item-center justify-center  w-120 '>
-              <div className='pl-8'>
+              <div className='pl-8 pb-4'>
                 <div className="flex item-center w-60">              
                   <textarea className='ml-2 rounded-xl border border-gray-600  w-60 pt-1 pl-2' placeholder='whats on your mind ' value={thoughtContent} onChange={e=>setThoughtContent(e.target.value)}/>
                 </div>  
@@ -49,10 +47,10 @@ export default function Home() {
               </div>
               
               <div>
-                <div className='mt-2  bg-red-400 text-white mr-8 rounded pt-1 pb-2'>
+                <div className='mt-2  bg-red-400 text-white  rounded pt-1 '>
                   {
                     thoughts.map(thought => (  
-                      <span className='ml-3 mr-3'>
+                      <span className='ml-3 mr-2'>
                         {thought.content } <br />
                       </span>
                       
@@ -69,7 +67,7 @@ export default function Home() {
       
       </main>
 
-      <footer className="flex h-24 w-full items-center justify-center border-t ">
+      <footer className="flex h-20 w-full items-center justify-center border-0 text-white ">
         <a
           className="flex items-center justify-center gap-2"
           rel="noopener noreferrer"
