feb105e1cbd4f60d964819976ffe9b9a6ba5d0fe
om2137 <omraut961@gmail.com>
2022-07-23 12:54:41 +0530

    navbar component added, muicore added(package)

--- dapp/pages/profile.tsx
+++ dapp/pages/profile.tsx
@@ -7,26 +7,37 @@ import SignUpForm from '../components/SignUpForm'
 import useThoughts from '../hooks/useThoughts'
 import MediaCard from '../components/Cards';
 import { ClassNames } from '@emotion/react';
+import Navbar from '../components/Navbar';
 
 const Profile = () => {
   const {connect, account, user, createUser, postThought, thoughts, } = useThoughts();  
   const [thoughtContent, setThoughtContent] = useState<string>('');
   return (
     <>
+      <header className=" bg-gray-900">
+        <Navbar
+          title='Thoughts'
+          name={user?.username}
+        />
+      </header>
       <main className="flex min-h-screen flex-col items-center bg-gray-900">
+        
         <div className='flex pt-20'>
           <div>
-            <img src={user?.avatar}  className="rounded-full mr-2 aspect-square" width="170" height="170"/>
-          </div>
-          <div className=''>
-            <span className='font-bold text-9xl uppercase text-white pl-10'>{user?.name}</span>
+            <img src={user?.avatar}  className="border-2 rounded-full mr-2 aspect-square" width="170" height="170"/>
           </div>
           <div>
-            <a href=".." className='flex  justify-center font-bold text-white ml-10 border rounded p-2'>BACK</a>
+            <div className=''>
+              <span className='font-bold text-9xl uppercase text-white pl-10'>{user?.name}</span>
+            </div>
+            <div className=''>
+              <span className='font-semibold text-4xl text-white pl-10 ml-5'>@{user?.username}</span>
+            </div>
           </div>
+          
         </div>
         
-        <div>
+        <div className='py-10'>
           {/* current loged in users content*/}
           {
             thoughts.map( thought => (
